b0f550341d7bd9b619b6077dc2878b11822524be
Marc Welz <68916291+marcwelz@users.noreply.github.com>
2023-09-30 15:12:46 +0200

    update controller

--- src/main/java/com/ch/marcwelz/featuretogglejava/controller/FeatureController.java
+++ src/main/java/com/ch/marcwelz/featuretogglejava/controller/FeatureController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/rest/opc/v1/features")
+@RequestMapping("/rest/features")
 public class FeatureController {
 }
